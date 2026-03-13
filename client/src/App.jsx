27a004c71788c7c3d1867b182a3e60bdd30c8a85
YahyaDumba <yahyadumba63@gmail.com>
2026-03-13 16:53:15 +0530

    Setup react client(frontend) with vite and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+   return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working! 🎉
+      </h1>
+    </div>
+  )
+
+}
+
+export default App;
